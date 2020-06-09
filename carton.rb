ea9443f1724b35d0a878623181c50da9b95c649a
Max Desiatov <max@desiatov.com>
2020-06-09 11:09:05 +0100

    Create carton.rb

---
+++ carton.rb
@@ -0,0 +1,23 @@
+class Carton < Formula
+  desc "📦 Watcher, bundler, and test runner for your SwiftWasm apps"
+  homepage "https://carton.dev"
+  head "https://github.com/swiftwasm/carton.git"
+
+  depends_on :xcode => "11.4"
+
+  stable do
+    version "0.0.3"
+    url "https://github.com/swiftwasm/carton/archive/#{version}.tar.gz"
+    sha256 "2a6ccbf3f8b8f3c4c84af2c153f2e751ab6debd1"
+  end
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    system "mv", ".build/release/carton", "carton"
+    bin.install "carton"
+  end
+
+  test do
+    system "carton -h"
+  end
+end
