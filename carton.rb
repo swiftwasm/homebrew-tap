33b12214a0109e1227b4d99dfa4cb945ea80c539
Max Desiatov <max@desiatov.com>
2020-07-21 22:29:23 +0100

    Fix wasmer dependency for the 0.4.0 version

--- carton.rb
+++ carton.rb
@@ -4,7 +4,7 @@ class Carton < Formula
   head "https://github.com/swiftwasm/carton.git"
 
   depends_on :xcode => "11.4"
-  depends_on :wasmer => "0.16.2"
+  depends_on "wasmer"
 
   stable do
     version "0.4.0"
