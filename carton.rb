4fa574ff03a4f6ed29ed3268af840985e95ebdde
Max Desiatov <max@desiatov.com>
2020-10-22 18:09:53 +0100

    Add bottle for carton 0.7.1 on macOS Catalina

--- carton.rb
+++ carton.rb
@@ -14,6 +14,12 @@ class Carton < Formula
     sha256 "157a480bc6e1eb3c88fd090b43b7a930890b42873305a41104e28104b2579aba"
   end
 
+  bottle do
+    root_url "https://github.com/swiftwasm/carton/releases/download/0.7.1"
+    cellar :any
+    sha256 "0ec64f927675d38d136bc1f27dab1aa313b7561595aec51c6e22882b3c2b7f26" => :catalina
+  end
+
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
     system "mv", ".build/release/carton", "carton"
