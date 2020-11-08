c46c4b76bddffdf31d555a69cf0e4271628bab6b
Yuta Saito <kateinoigakukun@gmail.com>
2020-11-08 09:50:00 +0900

    Release carton 0.8.0

--- carton.rb
+++ carton.rb
@@ -6,18 +6,11 @@ class Carton < Formula
   depends_on :xcode => "11.4"
   depends_on "wasmer"
   depends_on "binaryen"
-  depends_on "wabt"
 
   stable do
-    version "0.7.1"
+    version "0.8.0"
     url "https://github.com/swiftwasm/carton/archive/#{version}.tar.gz"
-    sha256 "157a480bc6e1eb3c88fd090b43b7a930890b42873305a41104e28104b2579aba"
-  end
-
-  bottle do
-    root_url "https://github.com/swiftwasm/carton/releases/download/0.7.1"
-    cellar :any
-    sha256 "0ec64f927675d38d136bc1f27dab1aa313b7561595aec51c6e22882b3c2b7f26" => :catalina
+    sha256 "d678aba99a1240fb5b68898509f2c8e40b02d23e6fe199eafa2ee95ba43c1079"
   end
 
   def install
