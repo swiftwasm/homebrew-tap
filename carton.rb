ca10208b4cd9513ee3e708a7b5a4d19c307aa55c
Max Desiatov <max@desiatov.com>
2020-11-09 15:00:28 +0000

    Fix `carton.rb` formula version

--- carton.rb
+++ carton.rb
@@ -8,12 +8,10 @@ class Carton < Formula
   depends_on "binaryen"
 
   stable do
-    version "0.8.1"
+    version "0.8.2"
     url "https://github.com/swiftwasm/carton.git", tag: "0.8.2", revision: "90d072a8897eb3e958314fe482f36d0ceb3e8dc5"
     sha256 "bdf9e259e9abb9d2ff9b68380b08a29c7c4f6a4b1114473593c3fc8afe33e574"
   end
-
-  
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
