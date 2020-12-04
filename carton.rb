7ecd5c11eb812add3cd04b65fdc59a8859acfe79
Max Desiatov <max@desiatov.com>
2020-12-04 13:10:40 +0000

    Update carton to 0.9.0

--- carton.rb
+++ carton.rb
@@ -8,16 +8,12 @@ class Carton < Formula
   depends_on "binaryen"
 
   stable do
-    version "0.8.2"
-    url "https://github.com/swiftwasm/carton.git", tag: "0.8.2", revision: "90d072a8897eb3e958314fe482f36d0ceb3e8dc5"
+    version "0.9.0"
+    url "https://github.com/swiftwasm/carton.git", tag: "0.9.0", revision: "a7314cbb2e4662efe2c2356ae327ffc6cb8de17e"
     sha256 "bdf9e259e9abb9d2ff9b68380b08a29c7c4f6a4b1114473593c3fc8afe33e574"
   end
 
-  bottle do
-    root_url "https://github.com/swiftwasm/carton/releases/download/0.8.2"
-    cellar :any
-    sha256 "7986d3a6340e0e9b385ec160f1ca9c39d840165bd9a415114f17cd3b936bc6fb" => :catalina
-  end
+  
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
