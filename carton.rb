fbb47787388fefa684f7ce295af114db184df08d
Simon Rice <im@simonrice.com>
2021-04-13 07:19:24 +0100

    Apply autocorrections from 'brew style --fix'

--- carton.rb
+++ carton.rb
@@ -1,11 +1,10 @@
+# typed: false
+# frozen_string_literal: true
+
 class Carton < Formula
   desc "📦 Watcher, bundler, and test runner for your SwiftWasm apps"
   homepage "https://carton.dev"
   head "https://github.com/swiftwasm/carton.git"
-
-  depends_on :xcode => "11.4"
-  depends_on "wasmer"
-  depends_on "binaryen"
 
   stable do
     version "0.9.1"
@@ -13,19 +12,24 @@ class Carton < Formula
     sha256 "bdf9e259e9abb9d2ff9b68380b08a29c7c4f6a4b1114473593c3fc8afe33e574"
   end
 
+  bottle do
+    root_url "https://github.com/swiftwasm/carton/releases/download/0.9.1"
+    sha256 cellar: :any, big_sur: "dd40aefac826c9739c81b69153326f023b8727839d41965ff63cf890734bca73"
+  end
+
+  depends_on "binaryen"
+
+  depends_on "wasmer"
+
+  depends_on xcode: "11.4"
+
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
     system "mv", ".build/release/carton", "carton"
     bin.install "carton"
   end
 
-  bottle do
-    root_url "https://github.com/swiftwasm/carton/releases/download/0.9.1"
-    cellar :any
-    sha256 "dd40aefac826c9739c81b69153326f023b8727839d41965ff63cf890734bca73" => :big_sur
-  end
-
   test do
-    system "carton -h"
+    system "carton", "-h"
   end
 end
