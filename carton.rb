63829d13165c5b7c7149068244a86fb8d15dd6fe
Max Desiatov <max@desiatov.com>
2022-04-30 12:44:33 +0100

    Add big_sur and arm64_monterey bottles for 0.14.2

--- carton.rb
+++ carton.rb
@@ -12,7 +12,12 @@ class Carton < Formula
 
     sha256 "bdf9e259e9abb9d2ff9b68380b08a29c7c4f6a4b1114473593c3fc8afe33e574"
     
-    
+    bottle do
+      root_url "https://github.com/swiftwasm/carton/releases/download/0.14.2"
+
+      sha256 cellar: :any, big_sur: "72fbaeb80e44f92dd038a96abc327d36b30539d2228aab8d93e67a069e6572c8"
+      sha256 cellar: :any, arm64_monterey: "c50367aed782811402a5cc2e114fc54da305a8f7dfae233ee4895b74b7f8608e"
+    end
   end
 
   depends_on "binaryen"
