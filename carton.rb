8ca67ff6c37ac97b1673111c029414c20f12a799
Max Desiatov <max@desiatov.com>
2022-05-23 10:57:00 +0100

    Update carton to 0.15.1

--- carton.rb
+++ carton.rb
@@ -7,17 +7,10 @@ class Carton < Formula
   head "https://github.com/swiftwasm/carton.git"
 
   stable do
-    version "0.15.0"
-    url "https://github.com/swiftwasm/carton.git", tag: "0.15.0", revision: "81308996282b5a92a09e4c55ceb985e48b192839"
+    version "0.15.1"
+    url "https://github.com/swiftwasm/carton.git", tag: "0.15.1", revision: "4314a92adc7b50764f0e7922955d6b8b3b90404b"
 
     sha256 "bdf9e259e9abb9d2ff9b68380b08a29c7c4f6a4b1114473593c3fc8afe33e574"
-
-    bottle do
-      root_url "https://github.com/swiftwasm/carton/releases/download/0.15.0"
-
-      sha256 cellar: :any, monterey: "f663eaaac2c4a867a98338c9064037397662d4baa8e95f1068e30487ba3af6b2"
-      sha256 cellar: :any, arm64_monterey: "5240c5446518defc7e6c78ae9520b701a77b2cabe2bc02e811a5c57c4e5db920"
-    end
   end
 
   depends_on "binaryen"
