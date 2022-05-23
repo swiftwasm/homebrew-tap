5e21b6f33f67ba5a9390d526b8c2d1430a135fa7
Max Desiatov <max@desiatov.com>
2022-05-23 16:50:53 +0100

    Add monterey and arm64_monterey bottles for 0.15.2

--- carton.rb
+++ carton.rb
@@ -11,6 +11,12 @@ class Carton < Formula
     url "https://github.com/swiftwasm/carton.git", tag: "0.15.2", revision: "8b44aa18b5de2b2ee34ac5e05182b4cbdd84b8ce"
 
     sha256 "bdf9e259e9abb9d2ff9b68380b08a29c7c4f6a4b1114473593c3fc8afe33e574"
+    bottle do
+      root_url "https://github.com/swiftwasm/carton/releases/download/0.15.2"
+      
+      sha256 cellar: :any, arm64_monterey: "7a308c64b42050896c70ed9d8a70ac1d67cd231f2e268c185d3534d5e25e99ca"
+      sha256 cellar: :any, monterey: "836a1a37a8af43b16d70f0a0eee07863e330d044d96ed898bff30b60c5244020"
+    end
   end
 
   depends_on "binaryen"
