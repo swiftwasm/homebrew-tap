8edb5d0d1ade255214f177d9ea5fdc93320ca4b0
Yuta Saito <kateinoigakukun@gmail.com>
2022-08-05 17:39:15 +0900

    carton 0.16.1
    * Revert "Revert "Update carton to 0.16.1""
      This reverts commit d1c6aa8ab8da866be8742fa1d0881d68a8ba6b0d.
    * Remove redundant version spec
    
    Closes #5.
    
    Signed-off-by: Yuta Saito <11702759+kateinoigakukun@users.noreply.github.com>

--- carton.rb
+++ carton.rb
@@ -7,16 +7,9 @@ class Carton < Formula
   head "https://github.com/swiftwasm/carton.git"
 
   stable do
-    version "0.16.0"
-    url "https://github.com/swiftwasm/carton.git", tag: "0.16.0", revision: "29a9b587b23e05653a6eab13af9ef77e4617f600"
+    url "https://github.com/swiftwasm/carton.git", tag: "0.16.1", revision: "9c0abbf013a01e163038f259d2787aa4baddbdcb"
 
     sha256 "bdf9e259e9abb9d2ff9b68380b08a29c7c4f6a4b1114473593c3fc8afe33e574"
-    bottle do
-      root_url "https://github.com/swiftwasm/carton/releases/download/0.16.0"
-      
-      sha256 cellar: :any, arm64_monterey: "60ff2f7d79146848bfd7853d2647a593d482f8601716d5e361530c4de613d7ce"
-      sha256 cellar: :any, monterey: "4dbb69d5cc861709566680e045cfcca2b16d1999a3867714a1591d7f39b155d3"
-    end
   end
 
   depends_on "binaryen"
