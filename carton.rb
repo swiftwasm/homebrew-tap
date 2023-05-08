7085ed1e98459413e8d55b935f934bd73c9875e8
Yuta Saito <kateinoigakukun@gmail.com>
2023-05-08 23:28:54 +0900

    Update carton to 0.19.0
    Closes #10.
    
    Signed-off-by: Yuta Saito <11702759+kateinoigakukun@users.noreply.github.com>

--- carton.rb
+++ carton.rb
@@ -7,14 +7,7 @@ class Carton < Formula
   head "https://github.com/swiftwasm/carton.git"
 
   stable do
-    url "https://github.com/swiftwasm/carton.git", tag: "0.18.0", revision: "f2a83a28bb03caa94784a4500e4f02f20b442306"
-
-    sha256 "bdf9e259e9abb9d2ff9b68380b08a29c7c4f6a4b1114473593c3fc8afe33e574"
-  end
-
-  bottle do
-    root_url "https://github.com/swiftwasm/homebrew-tap/releases/download/carton-0.18.0"
-    sha256 cellar: :any_skip_relocation, monterey: "c67e6aefd609dcad83792af00dd8753cd6d8554cce8542b30f0a04206608530a"
+    url "https://github.com/swiftwasm/carton.git", tag: "0.19.0", revision: "d2864bee744069cee3dbd816b07b160ce5e50dd3"
   end
 
   depends_on "binaryen"
