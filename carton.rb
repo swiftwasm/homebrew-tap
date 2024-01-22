281473e7fdf72e7646efb77aac4e227046c81bbe
Yuta Saito <kateinoigakukun@gmail.com>
2024-01-22 18:42:36 +0900

    Make brew cop happy

--- carton.rb
+++ carton.rb
@@ -1,14 +1,11 @@
-# typed: false
+# typed: true
 # frozen_string_literal: true
 
 class Carton < Formula
-  desc "📦 Watcher, bundler, and test runner for your SwiftWasm apps"
+  desc "Watcher, bundler, and test runner for your SwiftWasm apps"
   homepage "https://carton.dev"
+  url "https://github.com/swiftwasm/carton.git", tag: "0.20.0", revision: "71653a2baef83108ae27854bf98fc8604d97b43b"
   head "https://github.com/swiftwasm/carton.git"
-
-  stable do
-    url "https://github.com/swiftwasm/carton.git", tag: "0.20.0", revision: "71653a2baef83108ae27854bf98fc8604d97b43b"
-  end
 
   depends_on "binaryen"
 
