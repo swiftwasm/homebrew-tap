51b99f3c9a1433ad22f7eb694c4e62d30f48d7e3
Yuta Saito <kateinoigakukun@gmail.com>
2024-01-25 15:22:49 +0000

    carton: add 0.20.1 bottle.

--- carton.rb
+++ carton.rb
@@ -7,6 +7,11 @@ class Carton < Formula
   url "https://github.com/swiftwasm/carton.git", tag: "0.20.1", revision: "33ef59df6f00bf133f63cb0f4f21633ca8a4cfb4"
   head "https://github.com/swiftwasm/carton.git"
 
+  bottle do
+    root_url "https://github.com/swiftwasm/homebrew-tap/releases/download/carton-0.20.1"
+    sha256 cellar: :any_skip_relocation, ventura: "4738efe3e5aa6479b5b68c8457e27c16f02ab8fa76e06102b71e40cde890a4e9"
+  end
+
   depends_on "binaryen"
 
   depends_on "wasmer"
