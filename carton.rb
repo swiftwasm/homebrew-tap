7f57f3310913001225347841c27926951998f22e
Yuta Saito <kateinoigakukun@gmail.com>
2024-03-09 23:52:41 +0900

    Deprecate Homebrew installation

--- carton.rb
+++ carton.rb
@@ -12,6 +12,8 @@ class Carton < Formula
     sha256 cellar: :any_skip_relocation, ventura: "4738efe3e5aa6479b5b68c8457e27c16f02ab8fa76e06102b71e40cde890a4e9"
   end
 
+  deprecate! date: "2024-03-09", because: "deprecated Homebrew installation from 1.0.0. Please install the new version as a SwiftPM dependency instead (https://github.com/swiftwasm/carton#installation)"
+
   depends_on "binaryen"
 
   depends_on "wasmer"
